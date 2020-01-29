74d6813ab96c56b6fb2e149b76e2d937f0edfb7e
LHM <lhm@LHMui-MacBookPro.local>
2020-01-30 02:09:51 +0900

    2020-01-29

--- Weather.js
+++ Weather.js
@@ -1,20 +1,86 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 
-export default class Weather extends Component {
-  render() {
-    return (
-      <LinearGradient
-        colors={["#00C6FB", "#005BEA"]}
-        style={styles.container}
-      />
-    );
+const weatherOptions = {
+  Clouds: {
+    iconName: "cloud",
+    gradient: ["#bbb", "#333"]
+  },
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#3f2b96", "#a8c0ff"]
+  },
+  Drizzle: {
+    iconName: "",
+    gradient: ["", ""]
+  },
+  Rain: {
+    iconName: "",
+    gradient: ["", ""]
+  },
+  Snow: {
+    iconName: "",
+    gradient: ["", ""]
+  },
+  Atmosphere: {
+    iconName: "",
+    gradient: ["", ""]
+  },
+  Clear: {
+    iconName: "",
+    gradient: ["", ""]
   }
+};
+
+export default function Weather({ temp, condition }) {
+  return (
+    <LinearGradient
+      colors={weatherOptions[condition].gradient}
+      style={styles.container}
+    >
+      <View style={styles.harContainer}>
+        <StatusBar barStyle="light-content" />
+        <MaterialCommunityIcons
+          size={92}
+          color="#fff"
+          name={weatherOptions[condition].iconName}
+        />
+        <Text style={styles.font}>{temp} ᵒ</Text>
+      </View>
+      <View style={styles.harContainer}></View>
+    </LinearGradient>
+  );
 }
+
+Weather.propTypes = {
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds"
+  ]).isRequired
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  harContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  font: {
+    fontSize: 42,
+    color: "#fff"
   }
 });
